62ed9f153f5feb49740d0db776437e807cf03a69
huynguyen <nguyenhuytntc@gmail.com>
2022-06-10 16:36:52 +0700

    product new

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NewProducts from './NewProducts';
+import Policys from './Policys';
 import Slide from './Slide';
 
 function Home() {
     return (
         <div>
             <Slide />
+            <Policys />
+            <NewProducts />
         </div>
     );
 }
