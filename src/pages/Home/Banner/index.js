85de1dbac8036127a8aa4a365e950fd2ed2dede2
huynguyentntc <nguyenhuytntc@gmail.com>
2022-06-11 17:01:08 +0700

    commit

---
+++ src/pages/Home/Banner/index.js
@@ -0,0 +1,10 @@
+import classNames from 'classnames/bind';
+import styles from '.Banner.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Banner() {
+    return <div className={cx('wrapper')}>Banner</div>;
+}
+
+export default Banner;
--- src/pages/Home/Products/Product/index.js
+++ src/pages/Home/Products/Product/index.js
@@ -7,11 +7,16 @@ const cx = classNames.bind(styles);
 function Product() {
     return (
         <div className={cx('wrapper')}>
-            <Link to="/:productId">
+            <Link to="/:productId" className={cx('product-image')}>
                 <img
                     src="http://thaoduochuongque.com/thumbs/1366x435x1/upload/photo/slide-20610.png"
                     alt=""
                 />
+                <div className={cx('product-tag')}>
+                    <div className={cx('tag-sale')}>-11%</div>
+                    <div className={cx('tag-best-seller')}>Best seller</div>
+                    <div className={cx('tag-new')}>New</div>
+                </div>
             </Link>
             <div className={cx('description')}>
                 <Link to="/:productId">
@@ -19,9 +24,12 @@ function Product() {
                         Dầu gội sạch gàu - Bồ kết thảo dược
                     </h3>
                 </Link>
-                <p className={cx('price')}>
-                    150.000<sup>đ</sup>
-                </p>
+                <div className={cx('price')}>
+                    <p className={cx('price-now')}>
+                        150.000<sup>đ</sup>
+                    </p>
+                    <p className={cx('price-old')}>180.000</p>
+                </div>
             </div>
         </div>
     );
